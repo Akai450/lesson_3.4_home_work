652233c62f9deedb9c742dedfc1fb9271bff5ce8
Akai450 <alievakai@icloud.com>
2022-12-01 11:36:59 +0600

    Initial Commit

---
+++ lesson_3.4_home_work/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  lesson_3.4_home_work
+//
+//  Created by Akai on 1/12/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
